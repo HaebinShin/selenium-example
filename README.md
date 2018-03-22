a3eabb289688ec781288e0201a9e37018d60c0c5
Haebin Shin <sunsal0704@gmail.com>
2018-03-22 14:10:51 +0900

    Fix optional arguments

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 - chrome을 이용한 selenium 모듈 조작 예제
 - 주기적인 브라우저 조작 테스크를 crontab으로 예약
 
-## usage
+## Usage
 ### script.conf
-- user_data_dir: chrome://version에서 profile path에서 확인가능 (마지막 Default는 제외하고 dir만 적어주면 됨)
+run.py에서 사용되는 configuration 기본값 파일
 - dest: target이 될 사이트 주소
 - id: 컨트롤 할 html tag id
+- chromedriver_path: chromedriver의 위치
+    - ![https://sites.google.com/a/chromium.org/chromedriver/downloads](https://sites.google.com/a/chromium.org/chromedriver/downloads) 에서 다운로드
+    - 반드시 Supports Chrome을 확인하고 다운로드
+- profile_path: chrome://version에서 profile path에서 확인가능 (optional)
+    - 일반 chrome이 아닌 chromium의 경우엔 마지막 Default 이전까지 디렉토리 위치까지만 적어주면 됨
+- browser_bin_path: 브라우저 실행파일 위치 (optional)
+    - ubuntu의 경우, /usr/bin/google-chrome
 
 ### run.py
 - 사이트에서 버튼 조작 example
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from time import strftime, localtime
 def put_log(message):
 	time = strftime("%Y-%m-%d %H:%M:%S", localtime())
 	print("{} | {}".format(time, message))
-	
+
 
 from selenium import webdriver
 import selenium.webdriver.chrome.service as service
@@ -11,19 +11,32 @@ import configargparse
 import os
 parser = configargparse.ArgParser(default_config_files=[os.path.join(os.path.dirname(__file__), "script.conf"), "script.conf"])
 parser.add("-c", "--my-config", required=False, is_config_file=True, help="config file parserath")
-parser.add("--user-data-dir", dest="user_data_dir", help="user data dir for browser profile", required=True)
 parser.add("--dest", dest="dest", help="destination page", required=True)
 parser.add("--id", dest="id", help="manipulate html tag id", required=True)
+parser.add("--chromedriver-path", dest="chromedriver_path", help="chromedriver path", required=True)
+parser.add("--profile-path", dest="profile_path", help="user data dir for browser profile", default=None)
+parser.add("--browser-bin-path", dest="browser_bin_path", help="browser binary path", default=None)
 args = vars(parser.parse_args())
 
-capabilities = {'chrome.binary': '/usr/bin/google-chrome'}
+if args["browser_bin_path"] != None:
+    capabilities = {'chrome.binary': args["browser_bin_path"]}
+else:
+    capabilities = None
 
-options = webdriver.ChromeOptions() 
-options.add_argument("user-data-dir=" + args["user_data_dir"]) #Path to your chrome profile
+if args["profile_path"] != None:
+    options = webdriver.ChromeOptions() 
+    options.add_argument("user-data-dir=" + args["profile_path"]) #Path to your chrome profile
+else:
+    options = None
 
 put_log("start browser")
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", desired_capabilities=capabilities, chrome_options=options)
+driver = webdriver.Chrome(executable_path=args["chromedriver_path"], desired_capabilities=capabilities, chrome_options=options)
 driver.get(args["dest"])
+
+# Manipulate what you want.
+# e.g. how to fill form tag value?
+#   : driver.find_element_by_id("example").send_keys("blahblah")
+
 driver.find_element_by_id(args["id"]).click()
 put_log("finish")
-#driver.quit()
+driver.quit()
